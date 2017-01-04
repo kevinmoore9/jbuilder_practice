8e3c173760c56cfddf39c327ec9f2dd9791c1390
App Academy Student <student@appacademy.io>
2017-01-04 11:31:43 -0800

    add parties views and fix N+1 queries

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
   namespace :api, defaults: { format: :json } do
+    resources :parties, only: [:show, :index]
     resources :gifts, only: [:show]
     resources :guests, only: [:show, :index] do
       resources :gifts, only: [:index]
